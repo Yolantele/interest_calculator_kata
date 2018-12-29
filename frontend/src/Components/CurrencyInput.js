510e8cda4aa4b036db39c49b84d5c3a9a86caaec
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-12-29 10:41:16 +0000

    Installs all fornt-end dependencies

--- frontend/src/Components/CurrencyInput.js
+++ frontend/src/Components/CurrencyInput.js
@@ -15,6 +15,7 @@ export default class CurrencyInput extends Component {
 	handleChange(e) {
 		const value = e.target.value
 		this.setState({value})
+		console.log('---->', value)
 	}
 
 	handleFocus(e) {
